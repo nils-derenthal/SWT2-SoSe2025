3c98bedd86175b672274c18a196cda4718c7f9c2
Nils Derenthal <nils.derenthal@adesso.de>
2025-04-15 10:12:04 +0200

    setup: set up initial repo structure
    Co-authored-by: Maximizer02 <146656033+Maximizer02@users.noreply.github.com>

---
+++ schrottverwaltung/src/main/kotlin/de/fhdortmund/schrottverwaltung/SchrottverwaltungApplication.kt
@@ -0,0 +1,11 @@
+package de.fhdortmund.schrottverwaltung
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchrottverwaltungApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchrottverwaltungApplication>(*args)
+}
