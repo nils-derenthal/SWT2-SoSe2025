4b1b62af94bfa7c3ffdaf1cd03ed973c48a342f2
Netzlaw, Luis <Luis.Netzlaw@adesso.de>
2025-05-20 13:47:48 +0200

    SWTC-7:routing, main components and bootstrap fix (scss)

--- schrottportal/src/app/navbar/navbar.component.ts
+++ schrottportal/src/app/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
---
+++ schrottportal/src/app/uebersicht/uebersicht.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uebersicht',
+  imports: [],
+  templateUrl: './uebersicht.component.html',
+  styleUrl: './uebersicht.component.scss'
+})
+export class UebersichtComponent {
+
+}
