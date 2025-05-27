de021b8f0a8ee8560d60ef886a17f01eea2c9766
Netzluigi <netlawluiz@gmail.com>
2025-05-27 11:14:04 +0200

    7 navbar part 2 (#31)
    * SWTC-7: bootstrap install
    
    * SWTC-7:routing, main components and bootstrap fix (scss)
    
    * SWTC-7: styling
    
    * SWTC-7: remove workspace.xml
    
    ---------
    
    Co-authored-by: Netzlaw, Luis <Luis.Netzlaw@adesso.de>

---
+++ schrottportal/src/app/kalender/kalender.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kalender',
+  imports: [],
+  templateUrl: './kalender.component.html',
+  styleUrl: './kalender.component.scss'
+})
+export class KalenderComponent {
+
+}
