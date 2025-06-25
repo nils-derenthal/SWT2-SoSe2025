7a40cec5043002a819c89a192ff8c982c6ee6734
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-06-25 15:19:24 +0200

    karte der Immobilienansicht für die Übersichtsseite

--- schrottportal/src/app/uebersicht/uebersicht.component.ts
+++ schrottportal/src/app/uebersicht/uebersicht.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ImmoUebersichtskarteComponent} from './immo-uebersichtskarte/immo-uebersichtskarte.component';
 
 @Component({
   selector: 'app-uebersicht',
-  imports: [],
+  imports: [
+    ImmoUebersichtskarteComponent
+  ],
   templateUrl: './uebersicht.component.html',
   styleUrl: './uebersicht.component.scss'
 })
