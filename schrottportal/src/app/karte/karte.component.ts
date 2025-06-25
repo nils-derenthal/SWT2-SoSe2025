6cde1334228133a89d654fad1942137531d34144
Netzlaw, Luis <Luis.Netzlaw@adesso.de>
2025-06-25 15:42:24 +0200

    SWTB-5: get leaflet and first tests

--- schrottportal/src/app/karte/karte.component.ts
+++ schrottportal/src/app/karte/karte.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
+import {LeafletModule} from '@bluehalo/ngx-leaflet';
+import {latLng, marker, tileLayer} from 'leaflet';
 
 @Component({
   selector: 'app-karte',
-  imports: [],
+  imports: [
+    LeafletModule
+  ],
   templateUrl: './karte.component.html',
   styleUrl: './karte.component.scss',
 })
-export class KarteComponent {}
+export class KarteComponent implements OnInit{
+  osm = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'});
+  wikiM = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {maxZoom: 18});
+
+  testtext = signal('ungeklickt');
+
+  testmarker = marker([51.541944, 7.223889], {riseOnHover: true, title: this.testtext()});
+
+
+  markers = [this.testmarker, marker([51.5412, 7.2239]), marker([51.5419, 7.2238])];
+
+  layersControl = {
+    baseLayers: {
+      'Open Street Maps': this.osm,
+      'Wikimaps': this.wikiM,
+    },
+    overlays: {}
+  };
+
+  options = {
+    layers: [this.osm, ...this.markers],
+    zoom: 13,
+    center: latLng(51.541944, 7.223889)
+  };
+
+  ngOnInit(): void {
+    this.testmarker.on('click', (event) => {
+      console.log('ksdfl');
+      this.testtext.set('geklickt');
+    })
+  }
+}
