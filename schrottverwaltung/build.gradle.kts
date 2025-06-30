2ff477f1e9b5a2141d33c0c98fc799388b703361
Nils Derenthal <nils.derenthal@adesso.de>
2025-06-30 16:42:46 +0200

    wow4

--- schrottverwaltung/build.gradle.kts
+++ schrottverwaltung/build.gradle.kts
@@ -36,7 +36,3 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
