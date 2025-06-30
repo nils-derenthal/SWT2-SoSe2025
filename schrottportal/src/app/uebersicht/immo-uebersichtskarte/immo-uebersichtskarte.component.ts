cb0310ee855e2d8909ca1f0cd24e3bcfee0cf153
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-06-30 15:57:44 +0200

    This was bärbels commit (testing) (git probleme) (lol)

---
+++ schrottportal/src/app/uebersicht/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-immo-uebersichtskarte',
+  imports: [],
+  templateUrl: './immo-uebersichtskarte.component.html',
+  styleUrl: './immo-uebersichtskarte.component.scss'
+})
+export class ImmoUebersichtskarteComponent {
+
+}
