7ae2b7abd787a61e56c9df1fc87703007db9ca88
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-07-01 14:00:49 +0200

    service und controller

---
+++ schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/controller/ImmoStatusController.java
@@ -0,0 +1,19 @@
+package de.fhdortmund.schrottverwaltung.immobilie.controller;
+
+import de.fhdortmund.schrottverwaltung.immobilie.service.ImmoStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/immobilie/status")
+public class ImmoStatusController {
+    private final ImmoStatusService immoStatusService;
+
+    @GetMapping
+    public void getImmoStatus(Long immoId) {
+
+    }
+}
---
+++ schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/service/ImmoStatusService.java
@@ -0,0 +1,16 @@
+package de.fhdortmund.schrottverwaltung.immobilie.service;
+
+import de.fhdortmund.schrottverwaltung.immobilie.ImmoStatus;
+import de.fhdortmund.schrottverwaltung.immobilie.repo.ImmobilienStatusRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImmoStatusService {
+    private ImmobilienStatusRepo statusRepo;
+
+    public ImmoStatus getImmoStatus(Long id) {
+        return statusRepo.findImmobilienStatusById(id);
+    }
+}
