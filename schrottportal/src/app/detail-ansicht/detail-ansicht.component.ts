866948b2b808109b1881c3f55000c3b3a864ab9a
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-04 12:57:13 +0200

    komponente für detailansicht erstellt noch wip

--- schrottportal/src/app/detail-ansicht/detail-ansicht.component.ts
+++ schrottportal/src/app/detail-ansicht/detail-ansicht.component.ts
@@ -1,135 +1,47 @@
-import {Component, input, Input} from '@angular/core';
-import {ImmobilieDTO} from '../models/immobilie.model';
-import { Observable } from 'rxjs';
-import {ImmobilienService} from '../services/immobilien.service';
-import {AsyncPipe, NgClass} from '@angular/common';
-import {Adresse} from '../models/adresse.model';
-import {Koordinaten} from '../models/koordinaten.model';
-import {Bewertung} from '../models/bewertung.model';
-import {Eigentuemer} from '../models/eigentuemer.model';
-import {ImmoStatus} from '../models/immoStatus.model';
+import { Component, input, OnInit } from '@angular/core';
+import { ImmobilieDTO } from '../models/immobilie.model';
+import { map, Observable, switchMap } from 'rxjs';
+import { ImmobilienService } from '../services/immobilien.service';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { AmpelSliderComponent } from './ampel-slider/ampel-slider.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-detail-ansicht',
-  imports: [
-    AsyncPipe,
-    NgClass
-  ],
-  templateUrl: './detail-ansicht.component.html',
-  styleUrl: './detail-ansicht.component.scss'
+    selector: 'app-detail-ansicht',
+    imports: [AsyncPipe, NgClass, AmpelSliderComponent, RouterLink],
+    templateUrl: './detail-ansicht.component.html',
+    styleUrl: './detail-ansicht.component.scss'
 })
-export class DetailAnsichtComponent {
-  id = input.required<number>();
-  immobilie$!: Observable<ImmobilieDTO>;
-  value: string | undefined;
-  color: string | undefined;
+export class DetailAnsichtComponent implements OnInit {
+    id = input<number>();
+    immobilie$!: Observable<ImmobilieDTO>;
+    value: string | undefined;
+    color: string | undefined;
 
-  constructor(private immoService: ImmobilienService) {
-  }
-
-  ngOnInit(): void {
-    this.getImmobilieById();
-    this.value = "0";
-
-    switch (this.value) {
-      case '0':
-        this.color = 'red';
-        break;
-
-      case '1':
-        this.color = 'orange';
-        break;
-
-      case '2':
-        this.color = 'yellow';
-        break;
-
-      case '3':
-        this.color = 'green';
-        break;
-
-      case '4':
-        this.color = 'blue';
-        break;
+    constructor(private immoService: ImmobilienService, private router: ActivatedRoute) {
     }
-  }
 
-  getImmobilieById() {
-    this.immobilie$ = this.immoService.getImmobilieById(this.id());
-  }
+    possibleStatus: string[] = ["VERDACHT", "ANGEGUCKT", "ANHOERUNG", "WOHNUNGSAUFSICHT", "BESEITIGUNG", "NEUBAU", "NUTZUNG", "INSTANDSETZUNG", "ERWERB", "GELOEST"]
 
-
-  adresse: Adresse = {
-    id: 1,
-    strasse: "str",
-    hausnummer: 4,
-    hausnummerZusatz: "",
-    plz: 3333,
-    ort: "ort",
-    stadtbezirk: "bezirk"
-  }
-
-  eigentuemer: Eigentuemer = {
-    id: 3,
-    vorname: "Peter",
-    nachname: "Peter",
-    anschrift: this.adresse,
-    anmerkung: ""
-  }
-
-  status1: ImmoStatus = {
-    id: 1,
-    status: "status1",
-    beschreibung: "beschreibung1",
-    immobilieId: 1
-  }
-
-  status2: ImmoStatus = {
-    id: 2,
-    status: "status2",
-    beschreibung: "beschreibung2",
-    immobilieId: 1
-  }
-
-  status3: ImmoStatus = {
-    id: 3,
-    status: "status3",
-    beschreibung: "beschreibung3",
-    immobilieId: 1
-  }
-
-  status4: ImmoStatus = {
-    id: 4,
-    status: "status4",
-    beschreibung: "beschreibung4",
-    immobilieId: 1
-  }
-
-  status5: ImmoStatus = {
-    id: 5,
-    status: "status5",
-    beschreibung: "beschreibung5",
-    immobilieId: 1
-  }
-
-  @Input() immobilie: ImmobilieDTO = {
-    id: 1,
-    adresse: this.adresse,
-    bezeichnung: "bezeichnung",
-    archiviert: false,
-    zustand: "zustand",
-    koordinaten: {xKoordinate: 3, yKoordinate: 4, id: 9},
-    gemarkung: "gemarkung",
-    flur: "flur",
-    flurstueck: "flurstueck",
-    quadratMeter: 40,
-    gebaeudetyp: "typ",
-    eigentumsform: "eigentumsform",
-    bewertungen: [],
-    eigentuemer: this.eigentuemer,
-    immoStati: [this.status1, this.status2, this.status3, this.status4, this.status4, this.status5],
-    aktuellerStatusId: 3
-  };
-
-
+    ngOnInit(): void {
+        this.immobilie$ = this.router.params.pipe(map(params => (params as any)['id'] as number), switchMap(id => this.immoService.getImmobilieById(id)))
+        this.value = "0";
+        switch (this.value) {
+            case '0':
+                this.color = 'red';
+                break;
+            case '1':
+                this.color = 'orange';
+                break;
+            case '2':
+                this.color = 'yellow';
+                break;
+            case '3':
+                this.color = 'green';
+                break;
+            case '4':
+                this.color = 'blue';
+                break;
+        }
+    }
 }
