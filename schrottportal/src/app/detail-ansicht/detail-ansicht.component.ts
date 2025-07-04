32e58113953f528976fb0fb8e8c6d3a5086c5f15
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-04 12:57:13 +0200

    komponente für detailansicht erstellt noch wip

---
+++ schrottportal/src/app/detail-ansicht/detail-ansicht.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-ansicht',
+  imports: [],
+  templateUrl: './detail-ansicht.component.html',
+  styleUrl: './detail-ansicht.component.scss'
+})
+export class DetailAnsichtComponent {
+
+}
