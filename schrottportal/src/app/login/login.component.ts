5432465d5ee16991eb7c428ffe1c0fee270d5fcc
Nils Derenthal <nils.derenthal@adesso.de>
2025-07-04 10:33:03 +0200

    wow

--- schrottportal/src/app/login/login.component.ts
+++ schrottportal/src/app/login/login.component.ts
@@ -1,28 +1,38 @@
-import {Component, input, output} from '@angular/core';
+import {Component, inject, input, output} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatInput, MatInputModule, MatLabel} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, MatInput, MatFormField, MatLabel, MatInputModule],
+  imports: [ReactiveFormsModule, MatInput, MatFormField, MatLabel, MatInputModule, MatIcon],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  credentials = output<{
-    email: string,
-    password: string
-  }>();
-
-  type = input<'login' | 'register'>('login');
+  http = inject(HttpClient);
+  auth = inject(AuthService);
 
   login = new FormGroup({
     email: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
     password: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
   });
 
+  signup = new FormGroup({
+    email: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
+    firstname: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
+    lastname: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
+    password: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
+  });
+
   logIn() {
-    this.credentials.emit(this.login.getRawValue())
+
+  }
+
+  signUp() {
+    this.auth.signup(this.signup.getRawValue());
   }
 }
