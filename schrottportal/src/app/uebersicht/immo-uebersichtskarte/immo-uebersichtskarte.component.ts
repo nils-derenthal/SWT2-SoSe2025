242e6a1e237c4135650e923b26d12583bb089fe2
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-07-04 13:15:51 +0200

    unused imort raus

--- schrottportal/src/app/uebersicht/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
+++ schrottportal/src/app/uebersicht/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {ImmobilieDTO} from "../../models/immobilie.model";
-import {AmpelSliderComponent} from "../../detailansicht/ampel-slider/ampel-slider.component";
 
 @Component({
     selector: 'app-immo-uebersichtskarte',
