709fd4f2a3a30e9d4fdbc7b79048b13ec1015716
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-07 18:17:59 +0200

    doppelte detailansicht komponente gelöscht, ampel-slider in richtigen ordner verschoben, detail-ansicht struktur fertig (hat noch placeholder, funktionalität ist separates ticket)

--- schrottportal/src/app/app.component.ts
+++ schrottportal/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
-import {LoginComponent} from "./login/login.component";
-import { DetailansichtComponent } from './detailansicht/detailansicht.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
