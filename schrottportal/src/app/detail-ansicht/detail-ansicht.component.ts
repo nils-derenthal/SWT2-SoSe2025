2ccccab580c874a929005d54e3ee2e33cf956224
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-07 18:17:59 +0200

    doppelte detailansicht komponente gelöscht, ampel-slider in richtigen ordner verschoben, detail-ansicht struktur fertig (hat noch placeholder, funktionalität ist separates ticket)

--- schrottportal/src/app/detail-ansicht/detail-ansicht.component.ts
+++ schrottportal/src/app/detail-ansicht/detail-ansicht.component.ts
@@ -4,13 +4,19 @@ import { map, Observable, switchMap } from 'rxjs';
 import { ImmobilienService } from '../services/immobilien.service';
 import { AsyncPipe, NgClass } from '@angular/common';
 import { AmpelSliderComponent } from './ampel-slider/ampel-slider.component';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink} from '@angular/router';
+
 
 @Component({
-    selector: 'app-detail-ansicht',
-    imports: [AsyncPipe, NgClass, AmpelSliderComponent, RouterLink],
-    templateUrl: './detail-ansicht.component.html',
-    styleUrl: './detail-ansicht.component.scss'
+  selector: 'app-detail-ansicht',
+  imports: [
+    AsyncPipe,
+    NgClass,
+    AmpelSliderComponent,
+    RouterLink
+  ],
+  templateUrl: './detail-ansicht.component.html',
+  styleUrl: './detail-ansicht.component.scss'
 })
 export class DetailAnsichtComponent implements OnInit {
     id = input<number>();
@@ -18,30 +24,48 @@ export class DetailAnsichtComponent implements OnInit {
     value: string | undefined;
     color: string | undefined;
 
-    constructor(private immoService: ImmobilienService, private router: ActivatedRoute) {
-    }
+  constructor(private immoService: ImmobilienService, private router:ActivatedRoute) {
+  }
 
-    possibleStatus: string[] = ["VERDACHT", "ANGEGUCKT", "ANHOERUNG", "WOHNUNGSAUFSICHT", "BESEITIGUNG", "NEUBAU", "NUTZUNG", "INSTANDSETZUNG", "ERWERB", "GELOEST"]
 
-    ngOnInit(): void {
-        this.immobilie$ = this.router.params.pipe(map(params => (params as any)['id'] as number), switchMap(id => this.immoService.getImmobilieById(id)))
-        this.value = "0";
-        switch (this.value) {
-            case '0':
-                this.color = 'red';
-                break;
-            case '1':
-                this.color = 'orange';
-                break;
-            case '2':
-                this.color = 'yellow';
-                break;
-            case '3':
-                this.color = 'green';
-                break;
-            case '4':
-                this.color = 'blue';
-                break;
-        }
+  possibleStatus: string[] = ["VERDACHT",
+    "ANGEGUCKT",
+    "ANHOERUNG",
+    "WOHNUNGSAUFSICHT",
+    "BESEITIGUNG",
+    "NEUBAU",
+    "NUTZUNG",
+    "INSTANDSETZUNG",
+    "ERWERB",
+    "GELOEST"]
+
+  ngOnInit(): void {
+    this.immobilie$ = this.router.params.pipe(
+      map(params=> (params as any)['id']as number),
+      switchMap(id =>  this.immoService.getImmobilieById(id))
+    )
+    this.value = "0";
+
+    switch (this.value) {
+      case '0':
+        this.color = 'red';
+        break;
+
+      case '1':
+        this.color = 'orange';
+        break;
+
+      case '2':
+        this.color = 'yellow';
+        break;
+
+      case '3':
+        this.color = 'green';
+        break;
+
+      case '4':
+        this.color = 'blue';
+        break;
     }
+  }
 }
