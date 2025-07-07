eb7d138052e764201cab8bb5bd6e10e63f8d87fd
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-07-07 14:36:47 +0200

    ac

--- schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/controller/ImmoStatusController.java
+++ schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/controller/ImmoStatusController.java
@@ -1,22 +1,12 @@
 package de.fhdortmund.schrottverwaltung.immobilie.controller;
 
-import de.fhdortmund.schrottverwaltung.immobilie.entity.ImmoStatus;
-import de.fhdortmund.schrottverwaltung.immobilie.service.ImmoStatusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/immobilie/status")
 public class ImmoStatusController {
-    private final ImmoStatusService immoStatusService;
 
-    @GetMapping
-    public List<ImmoStatus> getImmoStatuses() {
-        return immoStatusService.getAllImmoStatuses();
-    }
 }
--- schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/service/ImmoStatusService.java
+++ schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/immobilie/service/ImmoStatusService.java
@@ -1,18 +1,16 @@
 package de.fhdortmund.schrottverwaltung.immobilie.service;
 
-import de.fhdortmund.schrottverwaltung.immobilie.entity.ImmoStatus;
+import de.fhdortmund.schrottverwaltung.immobilie.ImmoStatusEnum;
 import de.fhdortmund.schrottverwaltung.immobilie.repo.ImmobilienStatusRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ImmoStatusService {
     private ImmobilienStatusRepo statusRepo;
 
-    public List<ImmoStatus> getAllImmoStatuses() {
-        return statusRepo.findAll();
+    public ImmoStatusEnum[] getAllImmoStatuses() {
+        return ImmoStatusEnum.values();
     }
 }
