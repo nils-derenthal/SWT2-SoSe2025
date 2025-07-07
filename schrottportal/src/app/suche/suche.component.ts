0eee64f66f25e9f4daa6484ec6ecc0811e29e424
Netzluigi <netlawluiz@gmail.com>
2025-07-07 09:33:05 +0200

    Dseign der suche (#55)
    * SWTB-47: add card to suche
    
    * SWTB-47: add card to suche and small dseign changes
    
    * SWTB-47: remove old testdata
    
    ---------
    
    Co-authored-by: Netzlaw, Luis <Luis.Netzlaw@adesso.de>

--- schrottportal/src/app/suche/suche.component.ts
+++ schrottportal/src/app/suche/suche.component.ts
@@ -1,13 +1,18 @@
-import { Component, inject, model } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ImmobilienService } from '../services/immobilien.service';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { debounceTime, distinctUntilChanged, startWith, switchMap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import {Component, inject, model} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ImmobilienService} from '../services/immobilien.service';
+import {toObservable} from '@angular/core/rxjs-interop';
+import {debounceTime, distinctUntilChanged, startWith, switchMap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {ImmoUebersichtskarteComponent} from '../uebersicht/immo-uebersichtskarte/immo-uebersichtskarte.component';
 
 @Component({
   selector: 'app-suche',
-  imports: [FormsModule, AsyncPipe],
+  imports: [
+    FormsModule,
+    AsyncPipe,
+    ImmoUebersichtskarteComponent
+  ],
   templateUrl: './suche.component.html',
   standalone: true,
   styleUrl: './suche.component.scss',
