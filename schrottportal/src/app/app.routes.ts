26ccde886757f0022526dfc4fa793f395c873add
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-07 13:42:32 +0200

    detail-ansicht

--- schrottportal/src/app/app.routes.ts
+++ schrottportal/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { UebersichtComponent } from './uebersicht/uebersicht.component';
-import { SucheComponent } from './suche/suche.component';
-import { KarteComponent } from './karte/karte.component';
-import { KalenderComponent } from './kalender/kalender.component';
-import { ArchivComponent } from './archiv/archiv.component';
+import {UebersichtComponent} from './uebersicht/uebersicht.component';
+import {SucheComponent} from './suche/suche.component';
+import {KarteComponent} from './karte/karte.component';
+import {KalenderComponent} from './kalender/kalender.component';
+import {ArchivComponent} from './archiv/archiv.component';
 import {LoginComponent} from "./login/login.component";
+import {DetailAnsichtComponent} from './detail-ansicht/detail-ansicht.component';
 
 export const routes: Routes = [
   { path: 'uebersicht', component: UebersichtComponent },
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: 'karte', component: KarteComponent },
   { path: 'kalender', component: KalenderComponent },
   { path: 'archiv', component: ArchivComponent },
+  { path: 'detail-ansicht/:id', component: DetailAnsichtComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'uebersicht' },
 ];
