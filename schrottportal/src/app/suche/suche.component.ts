db0d6e51a1d903bacbc0c3136a988a44b8dc9230
keliakopoulos <katerina.eliakopoulos@adesso.de>
2025-07-11 13:37:55 +0200

    status request vereinfachen

--- schrottportal/src/app/suche/suche.component.ts
+++ schrottportal/src/app/suche/suche.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {ImmobilienService} from '../services/immobilien.service';
 import {toObservable} from '@angular/core/rxjs-interop';
-import {debounceTime, distinctUntilChanged, Observable, startWith, switchMap} from 'rxjs';
+import {debounceTime, distinctUntilChanged, startWith, switchMap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 import {ImmoUebersichtskarteComponent} from '../uebersicht/immo-uebersichtskarte/immo-uebersichtskarte.component';
 import {ImmoStatusService} from '../services/immoStatus.service';
@@ -18,28 +18,20 @@ import {ImmoStatusService} from '../services/immoStatus.service';
   standalone: true,
   styleUrl: './suche.component.scss',
 })
-export class SucheComponent implements OnInit {
+export class SucheComponent {
 
   immobilienService = inject(ImmobilienService);
   statusService = inject(ImmoStatusService);
-  stati$!: Observable<string[]>;
+  stati$ = this.statusService.getAllStati();
   aktiverFilter = signal<string>('')
   search = signal<string>('');
 
-
-  ngOnInit(): void {
-    this.getStati();
-  }
   immobilien$ = toObservable((this.search)).pipe(
     startWith(''),
     distinctUntilChanged(),
     debounceTime(300),
     switchMap((search) => this.immobilienService.getImmobilienBySearchAndFilter(search, this.aktiverFilter())),
   );
-
-  getStati(): void {
-    this.stati$ = this.statusService.getAllStati();
-  }
 
   doFilter(status: string):void {
     this.aktiverFilter.set(status);
