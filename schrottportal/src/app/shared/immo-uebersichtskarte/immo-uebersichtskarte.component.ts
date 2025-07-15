f92a7e3ab80202e691afbd9d624a924db65f360e
BergmannNina <131527346+BergmannNina@users.noreply.github.com>
2025-07-15 14:08:44 +0200

    bild anzeigen (#81)
    Co-authored-by: ADESSO\nbergmann <nina.bergmann@adesso.de>

--- schrottportal/src/app/shared/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
+++ schrottportal/src/app/shared/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { ImmobilieDTO } from '../../models/immobilie.model';
-import {NgOptimizedImage} from "@angular/common";
+import {Component, Input} from '@angular/core';
+import {ImmobilieDTO} from '../../models/immobilie.model';
 import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-immo-uebersichtskarte',
   imports: [
-    NgOptimizedImage,
     RouterLink
   ],
   templateUrl: './immo-uebersichtskarte.component.html',
   styleUrl: './immo-uebersichtskarte.component.scss',
+  standalone: true
 })
 export class ImmoUebersichtskarteComponent {
   @Input() immobilie!: ImmobilieDTO;
