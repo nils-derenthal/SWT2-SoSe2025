19bf2460d5aaed888ec3b9e76bf799b84eea6c17
ADESSO\nbergmann <nina.bergmann@adesso.de>
2025-07-15 15:45:46 +0200

    status in übersicht

--- schrottportal/src/app/shared/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
+++ schrottportal/src/app/shared/immo-uebersichtskarte/immo-uebersichtskarte.component.ts
@@ -1,16 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ImmobilieDTO} from '../../models/immobilie.model';
 import {RouterLink} from '@angular/router';
+import {map} from 'rxjs';
+import {LowerCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-immo-uebersichtskarte',
   imports: [
-    RouterLink
+    RouterLink,
+    LowerCasePipe
   ],
   templateUrl: './immo-uebersichtskarte.component.html',
   styleUrl: './immo-uebersichtskarte.component.scss',
   standalone: true
 })
-export class ImmoUebersichtskarteComponent {
+export class ImmoUebersichtskarteComponent implements OnInit {
   @Input() immobilie!: ImmobilieDTO;
+  protected readonly map = map;
+  activeState: string = '';
+
+  ngOnInit(): void {
+    this.immobilie.immoStati.forEach(value => {
+      if (value.id === this.immobilie.aktuellerStatusId) {
+        this.activeState = value.status;
+      }
+    })
+  }
+
+
 }
