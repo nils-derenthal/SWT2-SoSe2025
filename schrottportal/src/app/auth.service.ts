d4cd99d8ad673c1ae18d4b53d88d7f739a5503b2
Netzlaw, Luis <Luis.Netzlaw@adesso.de>
2025-07-15 09:27:50 +0200

    SWTB-75: add builder pattern for mitarbeiter

--- schrottportal/src/app/auth.service.ts
+++ schrottportal/src/app/auth.service.ts
@@ -1,20 +1,19 @@
-import {inject, Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
-import {catchError, map, Observable, of, single, tap, throwError} from "rxjs";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 
-export const AUTH_TOKEN = 'schrottverwaltung_auth'
+export const AUTH_TOKEN = 'schrottverwaltung_auth';
 
 type Credentials = {
-  email: string,
-  password: string,
-}
+  email: string;
+  password: string;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
   http = inject(HttpClient);
   router = inject(Router);
 
@@ -22,24 +21,32 @@ export class AuthService {
     localStorage.setItem(AUTH_TOKEN, btoa(`${creds.email}:${creds.password}`));
   }
 
-  redirect () {
-    this.router.navigate(['/uebersicht'])
-      .then();
+  deleteCredentials() {
+    localStorage.removeItem(AUTH_TOKEN);
+  }
+
+  redirect() {
+    this.router.navigate(['/uebersicht']).then();
   }
 
   login(creds: Credentials): Observable<void> {
     this.saveCredentials(creds);
-    return this.http.get<void>('/api/auth/validate').pipe(
-       tap(() => this.redirect()),
-    )
+    return this.http
+      .get<void>('/api/auth/validate')
+      .pipe(tap(() => this.redirect()));
   }
 
-  signup(dto: { email: string, firstname: string, lastname: string, password: string}): Observable<void> {
+  signup(dto: {
+    email: string;
+    firstname: string;
+    lastname: string;
+    password: string;
+  }): Observable<void> {
     return this.http.post<void>('/api/user/register', dto).pipe(
       tap(() => {
         this.saveCredentials(dto);
         this.redirect();
-      })
-    )
+      }),
+    );
   }
 }
--- schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/mitarbeiter/Mitarbeiter.java
+++ schrottverwaltung/src/main/java/de/fhdortmund/schrottverwaltung/mitarbeiter/Mitarbeiter.java
@@ -10,6 +10,10 @@ import lombok.*;
 @AllArgsConstructor
 public class Mitarbeiter {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String vorname;
 
     private String nachname;
@@ -20,7 +24,51 @@ public class Mitarbeiter {
 
     private Long fachbereichNr;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
+    public static class MitarbeiterBuilder {
+        private Long id;
+        private String vorname;
+        private String nachname;
+        private String passwort;
+        private String mail;
+        private Long fachbereichNr;
+
+        public MitarbeiterBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public MitarbeiterBuilder setVorname(String vorname) {
+            this.vorname = vorname;
+            return this;
+        }
+
+        public MitarbeiterBuilder setNachname(String nachname) {
+            this.nachname = nachname;
+            return this;
+        }
+
+        public MitarbeiterBuilder setPasswort(String passwort) {
+            this.passwort = passwort;
+            return this;
+        }
+
+        public MitarbeiterBuilder setMail(String mail) {
+            this.mail = mail;
+            return this;
+        }
+
+        public MitarbeiterBuilder setFachbereichNr(Long fachbereichNr) {
+            this.fachbereichNr = fachbereichNr;
+            return this;
+        }
+
+        public Mitarbeiter build() {
+            return new Mitarbeiter(id, vorname, nachname, passwort, mail, fachbereichNr);
+        }
+    }
+
+    public static MitarbeiterBuilder builder() {
+        return new MitarbeiterBuilder();
+    }
 }
